535a74c88eb4855aaafdf879079d96cc8a6da961
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2024-10-07 20:49:05 +0530

    ValidParentheses

---
+++ src/week1/ValidParentheses.java
@@ -0,0 +1,27 @@
+package week1;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0)
+            return false;
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty())
+                    return false;
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
